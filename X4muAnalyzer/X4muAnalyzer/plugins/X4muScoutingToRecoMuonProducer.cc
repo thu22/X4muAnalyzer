8ba6c2de0af4a35630a304e9e4959becf03720c9
未眠 <115171296+thu22@users.noreply.github.com>
2024-10-22 23:50:06 +0800

    Add files via upload

---
+++ X4muAnalyzer/X4muAnalyzer/plugins/X4muScoutingToRecoMuonProducer.cc
@@ -0,0 +1,346 @@
+/*
+ * Developed by Yiyang Zhao for Run-3 X-->J/psiJ/psi-->4mu Scouting Analysis
+ * 2024-10
+ * X4muScoutingToRecoMuonProducer unpacks Run3ScoutingMuon formats to reco format
+ * and cross link Run3ScoutingMuon - Run3ScoutingTrack
+*/
+
+#include <memory>
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/Utilities/interface/EDPutToken.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/Common/interface/OrphanHandle.h"
+
+//#include "DataFormats/Scouting/interface/Run3ScoutingElectron.h"
+#include "DataFormats/Scouting/interface/Run3ScoutingMuon.h"
+//#include "DataFormats/Scouting/interface/Run3ScoutingPFJet.h"
+#include "DataFormats/Scouting/interface/Run3ScoutingParticle.h"
+//#include "DataFormats/Scouting/interface/Run3ScoutingPhoton.h"
+#include "DataFormats/Scouting/interface/Run3ScoutingTrack.h"
+#include "DataFormats/Scouting/interface/Run3ScoutingVertex.h"
+
+//#include "DataFormats/JetReco/interface/PFJet.h"
+//#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+
+//#include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+//#include "fastjet/contrib/SoftKiller.hh"
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+#include "DataFormats/SiPixelDetId/interface/PixelSubdetector.h"
+#include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
+#include "DataFormats/TrackingRecHit/interface/TrackingRecHit.h"
+
+class X4muScoutingToRecoMuonProducer : public edm::stream::EDProducer<> {
+public:
+  explicit X4muScoutingToRecoMuonProducer(edm::ParameterSet const &iConfig);
+  ~X4muScoutingToRecoMuonProducer() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+  void beginStream(edm::StreamID) override {}
+  void produce(edm::Event &iEvent, edm::EventSetup const &setup) override;
+  void endStream() override {}
+
+  reco::Track createTrack(Run3ScoutingMuon const& scoutingMuon);
+  reco::Track createTrack(Run3ScoutingTrack const& scoutingTrack);
+  reco::Vertex createVertex(Run3ScoutingVertex const& scoutingVertex);
+  reco::Muon createMuon(Run3ScoutingMuon const& scoutingMuon);
+
+  void buildHitPattern(Run3ScoutingMuon const& scoutingMuon, Run3ScoutingTrack const& scoutingTrack, reco::Track & recoTrack);
+
+  void createMuons(edm::Handle<std::vector<Run3ScoutingMuon>> scoutingmuonHandle,
+                          std::unique_ptr<reco::MuonCollection> &scoutingmuons);
+  void createTracks(edm::Handle<std::vector<Run3ScoutingMuon>> scoutingmuonHandle,
+                           std::unique_ptr<reco::TrackCollection> &scoutingtracks);
+  
+  void clearVars();
+
+private:
+  const edm::EDGetTokenT<std::vector<Run3ScoutingMuon>> input_scoutingmuon_token_;
+  const edm::EDGetTokenT<std::vector<Run3ScoutingTrack>> input_scoutingtrack_token_;
+
+  //std::vector<reco::Muon> RecoMuon_;
+  std::vector<int> MuonCharge_;
+  std::vector<float> MuonPt_;
+  std::vector<float> MuonEta_;
+  std::vector<float> MuonPhi_;
+  //std::vector<int8_t> vertexIndex_;
+  std::vector<float> normchi2_;
+  std::vector<float> trkPt_;
+  std::vector<float> trkEta_;
+  std::vector<float> trkPhi_;
+};
+
+//
+// constructors and destructor
+//
+X4muScoutingToRecoMuonProducer::X4muScoutingToRecoMuonProducer( edm::ParameterSet const &iConfig)
+    : input_scoutingmuon_token_(consumes(iConfig.getParameter<edm::InputTag>("scoutingMuon"))),
+    input_scoutingtrack_token_(consumes(iConfig.getParameter<edm::InputTag>("scoutingTrack")))
+  {
+  //register products
+  //produceWithRef<reco::Muon, Run3ScoutingMuon>("recoMuons");
+  produces<reco::MuonCollection>("recoMuons");
+  produces<reco::TrackCollection>("recoTracks");
+  produces<edm::ValueMap<int>>("charge");
+  produces<edm::ValueMap<float>>("pt");
+  produces<edm::ValueMap<float>>("eta");
+  produces<edm::ValueMap<float>>("phi");
+  //produces<edm::ValueMap<std::vector<int>>>("vertexIndex");
+  produces<edm::ValueMap<float>>("normchi2");
+  produces<edm::ValueMap<float>>("trkPt");
+  produces<edm::ValueMap<float>>("trkEta");
+  produces<edm::ValueMap<float>>("trkPhi");
+}
+
+X4muScoutingToRecoMuonProducer::~X4muScoutingToRecoMuonProducer() = default;
+
+reco::Muon X4muScoutingToRecoMuonProducer::createMuon(Run3ScoutingMuon const& scoutingMuon) {
+  auto m = 0.1056583755;
+  auto q = scoutingMuon.charge();
+
+  float px = scoutingMuon.pt() * cos(scoutingMuon.phi());
+  float py = scoutingMuon.pt() * sin(scoutingMuon.phi());
+  float pz = scoutingMuon.pt() * sinh(scoutingMuon.eta());
+  float p = scoutingMuon.pt() * cosh(scoutingMuon.eta());
+  float energy = std::sqrt(p * p + m * m);
+  reco::Particle::LorentzVector p4(px, py, pz, energy);
+
+  static const reco::Muon dummy;
+  auto recomuon = reco::Muon(q, p4, createTrack(scoutingMuon).vertex());
+
+  //RecoMuon_.push_back(recomuon);
+  MuonCharge_.push_back(q);
+  MuonPt_.push_back(scoutingMuon.pt());
+  MuonEta_.push_back(scoutingMuon.eta());
+  MuonPhi_.push_back(scoutingMuon.phi());
+  //vertexIndex_.push_back(scoutingMuon.vtxIndx());
+  normchi2_.push_back(scoutingMuon.normalizedChi2());
+  trkPt_.push_back(scoutingMuon.trk_pt());
+  trkEta_.push_back(scoutingMuon.trk_eta());
+  trkPhi_.push_back(scoutingMuon.trk_phi());
+
+  //std::cout << "Muon px: " << px << std::endl;
+
+  return recomuon;
+}
+
+// ------------ method called to produce the data  ------------
+void X4muScoutingToRecoMuonProducer::produce(edm::Event &iEvent, edm::EventSetup const &setup) {
+  using namespace edm;
+
+  Handle<std::vector<Run3ScoutingMuon>> scoutingmuonHandle;
+  Handle<std::vector<Run3ScoutingTrack>> scoutingtrackHandle;
+  iEvent.getByToken(input_scoutingmuon_token_, scoutingmuonHandle);
+  iEvent.getByToken(input_scoutingtrack_token_, scoutingtrackHandle);
+
+  if(!scoutingmuonHandle.isValid() || !scoutingtrackHandle.isValid()) {
+    return;
+  }
+  //else std::cout << "ScoutingMuon is valid" << std::endl;
+
+  auto pfcands = std::make_unique<reco::MuonCollection>();
+  auto tkcands = std::make_unique<reco::TrackCollection>();
+
+  createMuons(scoutingmuonHandle, pfcands);
+  createTracks(scoutingmuonHandle, tkcands);
+
+  edm::OrphanHandle<reco::MuonCollection> oh = iEvent.put(std::move(pfcands), "recoMuons");
+  edm::OrphanHandle<reco::TrackCollection> oh2 = iEvent.put(std::move(tkcands), "recoTracks");
+
+  std::unique_ptr<edm::ValueMap<int>> charge_VM(new edm::ValueMap<int>());
+  edm::ValueMap<int>::Filler filler_charge(*charge_VM);
+  filler_charge.insert(oh, MuonCharge_.begin(), MuonCharge_.end());
+  filler_charge.fill();
+  iEvent.put(std::move(charge_VM), "charge");
+
+  std::unique_ptr<edm::ValueMap<float>> pt_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_pt(*pt_VM);
+  filler_pt.insert(oh, MuonPt_.begin(), MuonPt_.end());
+  filler_pt.fill();
+  iEvent.put(std::move(pt_VM), "pt");
+
+  std::unique_ptr<edm::ValueMap<float>> eta_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_eta(*eta_VM);
+  filler_eta.insert(oh, MuonEta_.begin(), MuonEta_.end());
+  filler_eta.fill();
+  iEvent.put(std::move(eta_VM), "eta");
+
+  std::unique_ptr<edm::ValueMap<float>> phi_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_phi(*phi_VM);
+  filler_phi.insert(oh, MuonPhi_.begin(), MuonPhi_.end());
+  filler_phi.fill();
+  iEvent.put(std::move(phi_VM), "phi");
+
+  /* std::unique_ptr<edm::ValueMap<int>> vertexIndex_VM(new edm::ValueMap<int>());
+  edm::ValueMap<int>::Filler filler_vertexIndex(*vertexIndex_VM);
+  filler_vertexIndex.insert(oh, vertexIndex_.begin(), vertexIndex_.end());
+  filler_vertexIndex.fill();
+  iEvent.put(std::move(vertexIndex_VM), "vertexIndex"); */
+
+  std::unique_ptr<edm::ValueMap<float>> normchi2_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_normchi2(*normchi2_VM);
+  filler_normchi2.insert(oh, normchi2_.begin(), normchi2_.end());
+  filler_normchi2.fill();
+  iEvent.put(std::move(normchi2_VM), "normchi2");
+
+  std::unique_ptr<edm::ValueMap<float>> trkPt_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_trkPt(*trkPt_VM);
+  filler_trkPt.insert(oh, trkPt_.begin(), trkPt_.end());
+  filler_trkPt.fill();
+  iEvent.put(std::move(trkPt_VM), "trkPt");
+
+  std::unique_ptr<edm::ValueMap<float>> trkEta_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_trkEta(*trkEta_VM);
+  filler_trkEta.insert(oh, trkEta_.begin(), trkEta_.end());
+  filler_trkEta.fill();
+  iEvent.put(std::move(trkEta_VM), "trkEta");
+
+  std::unique_ptr<edm::ValueMap<float>> trkPhi_VM(new edm::ValueMap<float>());
+  edm::ValueMap<float>::Filler filler_trkPhi(*trkPhi_VM);
+  filler_trkPhi.insert(oh, trkPhi_.begin(), trkPhi_.end());
+  filler_trkPhi.fill();
+  iEvent.put(std::move(trkPhi_VM), "trkPhi");
+
+  clearVars();
+}
+
+void X4muScoutingToRecoMuonProducer::clearVars() {
+  //RecoMuon_.clear();
+  MuonCharge_.clear();
+  MuonPt_.clear();
+  MuonEta_.clear();
+  MuonPhi_.clear();
+  //vertexIndex_.clear();
+  normchi2_.clear();
+  trkPt_.clear();
+  trkEta_.clear();
+  trkPhi_.clear();
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void X4muScoutingToRecoMuonProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("scoutingMuon", edm::InputTag("hltScoutingMuonPackerVtx"));
+  desc.add<edm::InputTag>("scoutingTrack", edm::InputTag("hltScoutingTrackPacker"));
+  descriptions.add("X4muScoutingToRecoMuonProducer", desc);
+}
+
+
+reco::Track X4muScoutingToRecoMuonProducer::createTrack(Run3ScoutingTrack const& scoutingTrack){
+    float chi2 = scoutingTrack.tk_chi2();
+    float ndof = scoutingTrack.tk_ndof();
+    reco::TrackBase::Point referencePoint(scoutingTrack.tk_vx(), scoutingTrack.tk_vy(), scoutingTrack.tk_vz());
+    
+    float px = scoutingTrack.tk_pt() * cos(scoutingTrack.tk_phi());
+    float py = scoutingTrack.tk_pt() * sin(scoutingTrack.tk_phi());
+    float pz = scoutingTrack.tk_pt() * sinh(scoutingTrack.tk_eta());
+    reco::TrackBase::Vector momentum(px, py, pz);
+    
+    int charge = scoutingTrack.tk_charge();
+    
+    std::vector<float> cov_vec(15); // 5*(5+1)/2 = 15
+    cov_vec[0] = scoutingTrack.tk_qoverp_Error() * scoutingTrack.tk_qoverp_Error(); // cov(0, 0)
+    cov_vec[1] = scoutingTrack.tk_qoverp_lambda_cov(); // cov(0, 1)
+    cov_vec[3] = scoutingTrack.tk_qoverp_phi_cov(); // cov(0, 2)
+    cov_vec[6] = scoutingTrack.tk_qoverp_dxy_cov(); // cov(0, 3)
+    cov_vec[10] = scoutingTrack.tk_qoverp_dsz_cov(); // cov(0, 4)
+    cov_vec[2] = scoutingTrack.tk_lambda_Error() * scoutingTrack.tk_lambda_Error(); // cov(1, 1)
+    cov_vec[4] = scoutingTrack.tk_lambda_phi_cov(); // cov(1, 2)
+    cov_vec[7] = scoutingTrack.tk_lambda_dxy_cov(); // cov(1, 3)
+    cov_vec[11] = scoutingTrack.tk_lambda_dsz_cov(); // cov(1, 4)
+    cov_vec[5] = scoutingTrack.tk_phi_Error() * scoutingTrack.tk_phi_Error(); // cov(2, 2) 
+    cov_vec[8] = scoutingTrack.tk_phi_dxy_cov(); // cov(2, 3)
+    cov_vec[12] = scoutingTrack.tk_phi_dsz_cov(); // cov(2, 4)
+    cov_vec[9] = scoutingTrack.tk_dxy_Error() * scoutingTrack.tk_dxy_Error(); // cov(3, 3)
+    cov_vec[13] = scoutingTrack.tk_dxy_dsz_cov(); // cov(3, 4)
+    cov_vec[14] = scoutingTrack.tk_dsz_Error() * scoutingTrack.tk_dsz_Error(); // cov(4, 4)
+    reco::TrackBase::CovarianceMatrix cov(cov_vec.begin(), cov_vec.end());
+
+    reco::TrackBase::TrackAlgorithm algo(reco::TrackBase::undefAlgorithm); // undefined
+    reco::TrackBase::TrackQuality quality(reco::TrackBase::confirmed); // confirmed
+    
+    // the rests are default: t0 = 0, beta = 0, covt0t0 = -1, covbetabeta = -1 
+  
+    reco::Track recoTrack(chi2, ndof, referencePoint, momentum, charge, cov, algo, quality);
+
+    return recoTrack;
+}
+
+reco::Track X4muScoutingToRecoMuonProducer::createTrack(Run3ScoutingMuon const& scoutingMuon){
+    float chi2 = scoutingMuon.trk_chi2();
+    float ndof = scoutingMuon.trk_ndof();
+    reco::TrackBase::Point referencePoint(scoutingMuon.trk_vx(), scoutingMuon.trk_vy(), scoutingMuon.trk_vz());
+    
+    float px = scoutingMuon.trk_pt() * cos(scoutingMuon.trk_phi());
+    float py = scoutingMuon.trk_pt() * sin(scoutingMuon.trk_phi());
+    float pz = scoutingMuon.trk_pt() * sinh(scoutingMuon.trk_eta());
+    reco::TrackBase::Vector momentum(px, py, pz);
+    
+    int charge = scoutingMuon.charge();
+    
+    std::vector<float> cov_vec(15); // 5*(5+1)/2 = 15
+    cov_vec[0] = scoutingMuon.trk_qoverpError() * scoutingMuon.trk_qoverpError(); // cov(0, 0)
+    cov_vec[1] = scoutingMuon.trk_qoverp_lambda_cov(); // cov(0, 1)
+    cov_vec[3] = scoutingMuon.trk_qoverp_phi_cov(); // cov(0, 2)
+    cov_vec[6] = scoutingMuon.trk_qoverp_dxy_cov(); // cov(0, 3)
+    cov_vec[10] = scoutingMuon.trk_qoverp_dsz_cov(); // cov(0, 4)
+    cov_vec[2] = scoutingMuon.trk_lambdaError() * scoutingMuon.trk_lambdaError(); // cov(1, 1)
+    cov_vec[4] = scoutingMuon.trk_lambda_phi_cov(); // cov(1, 2)
+    cov_vec[7] = scoutingMuon.trk_lambda_dxy_cov(); // cov(1, 3)
+    cov_vec[11] = scoutingMuon.trk_lambda_dsz_cov(); // cov(1, 4)
+    cov_vec[5] = scoutingMuon.trk_phiError() * scoutingMuon.trk_phiError(); // cov(2, 2) 
+    cov_vec[8] = scoutingMuon.trk_phi_dxy_cov(); // cov(2, 3)
+    cov_vec[12] = scoutingMuon.trk_phi_dsz_cov(); // cov(2, 4)
+    cov_vec[9] = scoutingMuon.trk_dxyError() * scoutingMuon.trk_dxyError(); // cov(3, 3)
+    cov_vec[13] = scoutingMuon.trk_dxy_dsz_cov(); // cov(3, 4)
+    cov_vec[14] = scoutingMuon.trk_dszError() * scoutingMuon.trk_dszError(); // cov(4, 4)
+    reco::TrackBase::CovarianceMatrix cov(cov_vec.begin(), cov_vec.end());
+
+    reco::TrackBase::TrackAlgorithm algo(reco::TrackBase::undefAlgorithm); // undefined
+    reco::TrackBase::TrackQuality quality(reco::TrackBase::confirmed); // confirmed
+    
+    // the rests are default: t0 = 0, beta = 0, covt0t0 = -1, covbetabeta = -1 
+  
+    reco::Track recoTrack(chi2, ndof, referencePoint, momentum, charge, cov, algo, quality);
+
+    return recoTrack;
+}
+
+void X4muScoutingToRecoMuonProducer::createMuons(
+    edm::Handle<std::vector<Run3ScoutingMuon>> scoutingmuonHandle,
+    std::unique_ptr<reco::MuonCollection> &scoutingmuons) {
+  for (unsigned int icand = 0; icand < scoutingmuonHandle->size(); ++icand) {
+    auto &scoutingmuon = (*scoutingmuonHandle)[icand];
+
+    auto pfcand = createMuon(scoutingmuon);
+    if (pfcand.energy() != 0)
+      scoutingmuons->push_back(pfcand);
+  }
+}
+
+void X4muScoutingToRecoMuonProducer::createTracks(
+    edm::Handle<std::vector<Run3ScoutingMuon>> scoutingmuonHandle,
+    std::unique_ptr<reco::TrackCollection> &scoutingtracks) {
+  for (unsigned int icand = 0; icand < scoutingmuonHandle->size(); ++icand) {
+    auto &scoutingmuon = (*scoutingmuonHandle)[icand];
+
+    auto tkcand = createTrack(scoutingmuon);
+    if (tkcand.pt() != 0)
+      scoutingtracks->push_back(tkcand);
+  }
+}
+
+// declare this class as a framework plugin
+DEFINE_FWK_MODULE(X4muScoutingToRecoMuonProducer);
---
+++ X4muAnalyzer/X4muAnalyzer/test/test_X4muAnalyzer.py
@@ -0,0 +1,122 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step2 -s NANO:@Scout --process NANO --data --eventcontent NANOAOD --datatier NANOAOD --era Run3_2024 --conditions 140X_dataRun3_Prompt_v4 --python_file scoutingnano_data_standalone.py -n 100 --fileout scouting_nano.root --filein root://cms-xrd-global.cern.ch//store/data/Run2024E/ScoutingPFRun3/HLTSCOUT/v1/000/381/594/00000/00d45875-ec52-4335-961c-adc18a956a9e.root
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
+
+process = cms.Process('NANO',Run3_2024)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('PhysicsTools.NanoAOD.custom_run3scouting_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+
+process.MessageLogger.cerr.FwkSummary.reportEvery = 1000000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/data/Run2024G/ScoutingPFRun3/HLTSCOUT/v1/000/385/312/00000/72e81dd1-3997-44ad-a166-dbc0bbb1d1f4.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    TryToContinue = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    modulesToCallForTryToContinue = cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(8),
+    numberOfThreads = cms.untracked.uint32(8),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step2 nevts: All'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAOD'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('dimuon_scouting.root'),
+    outputCommands = process.NANOAODEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_Prompt_v4', '')
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.scoutingNanoSequence)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step,process.NANOAODoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.NanoAOD.custom_run3scouting_cff
+from PhysicsTools.NanoAOD.custom_run3scouting_cff import customiseScoutingNanoAOD 
+
+#call to customisation function customiseScoutingNanoAOD imported from PhysicsTools.NanoAOD.custom_run3scouting_cff
+process = customiseScoutingNanoAOD(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
