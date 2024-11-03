8f224f7734a9eec06466d0f406112a35e3bf561f
未眠 <115171296+thu22@users.noreply.github.com>
2024-11-03 19:35:40 +0800

    X -- > Meson + Meson --> 4mu
    Version update

--- X4muAnalyzer/X4muAnalyzer/test/test_X4muAnalyzer.py
+++ X4muAnalyzer/X4muAnalyzer/test/test_X4muAnalyzer.py
@@ -21,17 +21,17 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_Prompt_v4', '')
 
-process.MessageLogger.cerr.FwkSummary.reportEvery = 100000000
-process.MessageLogger.cerr.FwkReport.reportEvery = 100000000
+process.MessageLogger.cerr.FwkSummary.reportEvery = 10000
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.maxEvents = cms.untracked.PSet( 
-    input = cms.untracked.int32(-1) 
+    input = cms.untracked.int32(-1) # -1 
 )
 
 process.source = cms.Source("PoolSource",
         # replace 'myfile.root' with the source file you want to use
         fileNames = cms.untracked.vstring(
-        'root://cms-xrd-global.cern.ch//store/data/Run2024G/ScoutingPFRun3/HLTSCOUT/v1/000/385/312/00000/72e81dd1-3997-44ad-a166-dbc0bbb1d1f4.root'
+        'root://cms-xrd-global.cern.ch/' + '/store/data/Run2024G/ScoutingPFRun3/HLTSCOUT/v1/000/385/312/00000/72e81dd1-3997-44ad-a166-dbc0bbb1d1f4.root'
         )
 )
 
@@ -86,6 +86,13 @@ process.X4muFilter = cms.EDFilter('CandViewCountFilter',
 process.X4muVertexFinder = cms.EDProducer("X4muSecondaryVertexProducer",
     recoMuon = cms.InputTag("X4muConverter", "recoMuons"),
     recoTrack = cms.InputTag("X4muConverter", "recoTracks"),
+    MesonMassBig = cms.double(3.0969), # J/Psi
+    MesonMassBigErr = cms.double(0.00004), # J/Psi
+    MesonMassSmall = cms.double(3.0969), # J/Psi
+    MesonMassSmallErr = cms.double(0.00004), # J/Psi
 )
+
+#MesonMassBig = cms.double(9.4604), # Upsilon
+#MesonMassBigErr = cms.double(0.0001), # Upsilon
 
 process.p = cms.Path(process.X4muConverter*process.X4muFilter*process.X4muVertexFinder)
